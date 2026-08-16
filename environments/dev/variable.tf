40533b51d0fb7c2ccc0567d86a955be074379ae0
Rakshit Tiwari <rakshittiwari@Rakshits-MacBook-Air.local>
2026-08-16 19:45:21 +0530

    Error: Vnet variable name was wrong under subnet variabe

--- environments/dev/variable.tf
+++ environments/dev/variable.tf
@@ -1,8 +1,26 @@
-variable "rg" {}
+variable "rg" {
+  type = object({
+    resource_group_name = string
+    location            = string
+  })
+}
+variable "vnet" {
+  type = object({
+    vnet_name           = string
+    resource_group_name = string
+    location            = string
+    address_space       = list(string)
+  })
+}
 
-variable "vnet" {}
-
-variable "subnet" {}
+variable "subnet" {
+  type = object({
+    subnet_name          = string
+    resource_group_name  = string
+    vnet_name = string
+    address_prefixes     = list(string)
+  })
+}
 
 variable "VM" {
   type = object({
