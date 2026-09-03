16dd6528e8f52f7f06a875c4bbf3c7b285196a83
Rakshit Tiwari <rakshittiwari@Rakshits-MacBook-Air.local>
2026-09-03 18:10:54 +0530

    fix: rename resource-group output from 'name' to 'resource_group_name'

--- modules/virtual_machine/main.tf
+++ modules/virtual_machine/main.tf
@@ -7,12 +7,6 @@ terraform {
   }
 }
 
-# Configure the Microsoft Azure Provider
-provider "azurerm" {
-  resource_provider_registrations = "none" # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
-  features {}
-}
-
 resource "azurerm_network_interface" "NIC" {
   name                = "${var.VM.prefix}-nic"
   location            = var.VM.location
